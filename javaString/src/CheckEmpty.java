8bf9e430a1fd6e128c9bbdf35522da652de23d4a
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-03-15 17:14:48 +0545

    check Empty in string

---
+++ javaString/src/CheckEmpty.java
@@ -0,0 +1,8 @@
+public class CheckEmpty {
+    public static void main(String[] args) {
+        String name = "Saugat";
+        String name1 = "";
+        System.out.println(name.isEmpty());
+        System.out.println(name1.isEmpty());
+    }
+}
