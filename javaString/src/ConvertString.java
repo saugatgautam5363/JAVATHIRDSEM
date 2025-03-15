714ec90b867917f1b90c6ac77d7dc6d4ae09b4e1
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-03-15 17:30:33 +0545

    String methos

---
+++ javaString/src/ConvertString.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class ConvertString {
+    public static void main(String[] args) {
+        String name = "saugat";
+        String convertToUpperCase = name.toUpperCase();
+        System.out.println(convertToUpperCase);
+        String name2 = "SANTOSH";
+        String convertToLowerCase = name2.toLowerCase();
+        System.out.println(convertToLowerCase);
+
+        String ignorChar = name.replace("saugat","Saugat");
+        System.out.println(ignorChar);
+
+        char s = name.charAt(3);
+        System.out.println(s);
+
+    }
+}
