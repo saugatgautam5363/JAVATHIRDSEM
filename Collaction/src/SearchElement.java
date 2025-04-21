3ce775a569beba716c7cceb9c7eb421961d72a9e
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-04-21 18:50:56 +0545

    search the element from arrayList
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ Collaction/src/SearchElement.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchElement {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(40);
+        list.add(9);
+        list.add(64);
+        list.add(4);
+
+        System.out.println("Search element: "+list.get(0));
+    }
+}
