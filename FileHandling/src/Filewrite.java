01aeb5a3e2825b78d9405f899fe176586f990d8f
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-05-08 21:48:25 +0545

    file write using java programming
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ FileHandling/src/Filewrite.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Filewrite {
+    public static void main(String[] args) {
+        try (FileWriter writer = new FileWriter("File.txt")) {
+            writer.write("Hello, Java file handling!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
