e51c5996efebb332e5e265d48a9ad36ecb226c09
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-08 09:40:52 +0545

    Iterating through a string
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ javaString/src/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        String str = "saugat";
+        for(int i = 0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
