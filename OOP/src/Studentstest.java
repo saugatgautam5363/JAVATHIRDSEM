06b91f8b04ec5337356caa68dcee1e9f1780d8de
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-13 18:48:06 +0545

    oop concept
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ OOP/src/Studentstest.java
@@ -0,0 +1,9 @@
+public class Studentstest {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.display();
+        System.out.println("-----------------------------------");
+        Student student1 = new Student("Saugat",18,"BCA");
+        student1.display();
+    }
+}
