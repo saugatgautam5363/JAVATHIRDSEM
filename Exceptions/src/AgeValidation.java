e466bb1c027eec5c4c318b6eeba6e6543d74059d
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-13 23:03:44 +0545

    custom exception in java programming
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ Exceptions/src/AgeValidation.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AgeValidation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            System.out.println("Enter the age: ");
+            int age = scanner.nextInt();
+
+            if(age < 0 || age<18){
+                throw new InvalidException("Invalid age");
+            }
+            System.out.println("Access granted. Age is valid.");
+        }catch(InvalidException e){
+            System.out.println("Custom exception: "+e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally {
+            scanner.close();
+        }
+    }
+}
