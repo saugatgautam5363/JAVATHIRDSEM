829276e1f84f4cfa3ca474a152390be6068eeece
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-13 20:02:40 +0545

    oop concept
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ OOP/src/InheritanceTest.java
@@ -0,0 +1,11 @@
+public class InheritanceTest {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Suman","1FD",450000);
+        employee.display();
+        System.out.println("---------------");
+        Manager manager = new Manager("saugat","AF1",10000000,"IT");
+        manager.display();
+        manager.manageTeam();
+
+    }
+}
