f402bb72356f40d0c27e2c03ec2390dd8b0cc0ae
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-17 19:19:34 +0545

    Serialization in jva programming example
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ test/src/StudentDeseri.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+
+public class StudentDeseri {
+    public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream("File.txt");
+            ObjectInputStream obj = new ObjectInputStream(file);
+
+            STudent s1 = (STudent)  obj.readObject();
+
+            System.out.println("Name: "+s1.name);
+            System.out.println("Roll: "+s1.roll);
+            System.out.println("Subject: "+s1.subject);
+            obj.close();;
+            file.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
