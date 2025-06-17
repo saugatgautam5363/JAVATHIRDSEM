bff2a7a84dedfc6acf63d865e102b141b90e7120
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-17 19:19:24 +0545

    Serialization in jva programming example
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

---
+++ test/src/STudent.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public class STudent implements Serializable {
+    String name;
+    int roll;
+    String subject;
+
+    public STudent(String name,int roll,String subject){
+        this.name = name;
+        this.roll = roll;
+        this.subject = subject;
+    }
+}
