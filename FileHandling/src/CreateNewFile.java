90e989d1f352fdeadacc757ac2bf505b770861da
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-19 13:37:23 +0545

    java
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

--- FileHandling/src/CreateNewFile.java
+++ FileHandling/src/CreateNewFile.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class CreateNewFile {
     public static void main(String[] args) {
-        File newFile = new File("File.txt");
+        File newFile = new File("student.ser");
         try{
                 newFile.createNewFile();
             } catch (IOException e) {
