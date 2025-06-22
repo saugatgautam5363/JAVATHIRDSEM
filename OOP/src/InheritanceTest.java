b5dd4fbe2b9ac589b6b10eb24270458d2b3e64e3
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-22 21:40:15 +0545

    college homework
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

--- OOP/src/InheritanceTest.java
+++ OOP/src/InheritanceTest.java
@@ -3,6 +3,7 @@ public class InheritanceTest {
         Employee employee = new Employee("Suman","1FD",450000);
         employee.display();
         System.out.println("---------------");
+
         Manager manager = new Manager("saugat","AF1",10000000,"IT");
         manager.display();
         manager.manageTeam();
