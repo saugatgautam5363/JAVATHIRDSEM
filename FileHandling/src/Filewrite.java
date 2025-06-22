5bab567aa8991f721699b49abf78f0a940f7bd80
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-06-22 21:38:02 +0545

    college homework
    Signed-off-by: saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>

--- FileHandling/src/Filewrite.java
+++ FileHandling/src/Filewrite.java
@@ -2,6 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Filewrite {
+
     public static void main(String[] args) {
         try (FileWriter writer = new FileWriter("File.txt")) {
             writer.write("Hello, Java file handling!");
